340d6fa8d7878133f5f0c562a612f1d23a721041
James Pittendreigh <james.pittedreigh@skybettingandgaming.com>
2019-08-15 19:08:09 +0100

    Adding few more files

--- fpinkotlin-parent/fpinkotlin-recursion-exercises/src/main/kotlin/com/fpinkotlin/recursion/exercise03/Fibonacci.kt
+++ fpinkotlin-parent/fpinkotlin-recursion-exercises/src/main/kotlin/com/fpinkotlin/recursion/exercise03/Fibonacci.kt
@@ -13,14 +13,9 @@ import java.math.BigInteger
 // n:  1 2 3 4 5 6 7  8
 // fib:1 1 2 3 5 8 13 21
 fun fib(x: Int): BigInteger {
-    tailrec fun go(nth: Int, lastTwo: Pair<BigInteger, BigInteger>): BigInteger {
-        return if (nth == x)
-            lastTwo.second
-        else {
-            val (a, b) = lastTwo
-            go(nth + 1, Pair(b, a + b))
-        }
-    }
+    tailrec fun go(n: Int, a: BigInteger, b: BigInteger): BigInteger =
+        if (n == x) b
+        else go(n.inc(), b, a + b)
 
-    return go(1, Pair(1.toBigInteger(), 1.toBigInteger()))
+    return go(1, 1.toBigInteger(), 1.toBigInteger())
 }
